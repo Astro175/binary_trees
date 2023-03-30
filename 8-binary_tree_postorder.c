8c6e7f72776cdc3402da24d3989be043976d12f7
Astro175 <aspark400@gmail.com>
2023-03-30 14:35:41 +0100

    done with task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - function that transverses a tree using postorder transversal
+ * @tree: tree to transverse
+ * @func: pointer to function that print node's data
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (func == NULL || tree == NULL)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
